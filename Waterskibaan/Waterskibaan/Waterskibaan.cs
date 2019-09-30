999b8015e099466671a955b61ff0eda33b0832e1
Justin Noppers <justin.noppers@gmail.com>
2019-09-30 21:54:39 +0200

    Opdracht 4 (Eindelijk)

--- Waterskibaan/Waterskibaan/Waterskibaan.cs
+++ Waterskibaan/Waterskibaan/Waterskibaan.cs
@@ -8,9 +8,36 @@ namespace Waterskibaan
 {
     class Waterskibaan
     {
+
+        private LijnenVoorraad lijnenVoorraad = new LijnenVoorraad();
+        private Kabel kabel = new Kabel();
+
+        public Waterskibaan(LijnenVoorraad lijnenVoorraad)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Lijn lijn = new Lijn(0);
+                lijnenVoorraad.LijnToevoegenAanRij(lijn);
+            }
+
+        }
+
         public void VerplaatsKabel()
         {
-            // TODO
+            kabel.VerschuifLijnen();
+            lijnenVoorraad.LijnToevoegenAanRij(kabel.VerwijderLijnVanKabel());
+        }
+
+        public override string ToString()
+        {
+            string voorraadLijn = lijnenVoorraad.ToString();
+            string overzichtKabel = kabel.ToString();
+            string print = "";
+
+            print += voorraadLijn;
+            print += overzichtKabel;
+
+            return print;
         }
     }
 }
